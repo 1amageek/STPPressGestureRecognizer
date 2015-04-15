675e8dae1c9254bf212fc4dec34831039ee35ccc
NORI <tmy0x3@icloud.com>
2015-04-15 22:52:14 +0900

    Initial Commit

---
+++ STPPressGestureRecognizerSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STPPressGestureRecognizerSample
+//
+//  Created by Norikazu on 2015/04/15.
+//  Copyright (c) 2015年 Stamp inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ STPPressGestureRecognizerSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  STPPressGestureRecognizerSample
+//
+//  Created by Norikazu on 2015/04/15.
+//  Copyright (c) 2015年 Stamp inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
